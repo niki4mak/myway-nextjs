ad90672d60158564de4d1b82c310a0be33b97582
s1mp0l <i.sidoruk03@gmail.com>
2024-09-07 21:11:08 +0300

    small temp fix images on main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default async function Entry() {
   return (
     <div className={"w-screen h-screen top-0 left-0 grid grid-cols-2"}>
       <Link href={"/home"} className={"relative h-full w-full flex items-center justify-center transition-all bg-black hover:bg-transparent"}>
-        <div className={"absolute h-full right-0"}>
+        <div className={"absolute h-full right-0 flex flex-col justify-end"}>
           <Image
-            className={"h-full w-fit object-fit object-right z-0"}
+            className={"h-[80%] w-fit object-cover object-right z-0"}
             width={1040}
             height={1960}
             src={"/landing/d-woman@2x.png"}
@@ -18,9 +18,9 @@ export default async function Entry() {
         <ButtonTransparent text={"Женское"}/>
       </Link>
       <Link href={"/home"} className={"relative w-full h-full flex items-center justify-center transition-all bg-black hover:bg-transparent"}>
-        <div className={"absolute h-full left-0"}>
+        <div className={"absolute h-full left-0 flex flex-col justify-end"}>
           <Image
-            className={"h-full w-fit object-cover object-left z-0"}
+            className={"h-[80%] w-fit object-cover object-left z-0"}
             width={1040}
             height={1960}
             src={"/landing/d-man@2x.png"}
