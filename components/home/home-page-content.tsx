18a22ec3e959075bce8a893aeb79bb14e1d3282b
rumpwar <niki4.mak@gmail.com>
2025-01-23 15:40:05 +0300

    test

--- components/home/home-page-content.tsx
+++ components/home/home-page-content.tsx
@@ -20,7 +20,7 @@ const HomePageContent = memo(() => {
 
       <div className={`w-full ${isMobile ? "" : "ml-[100%]"} flex flex-col items-center gap-2`}>
         <div className={"flex items-center font-display text-6xl justify-center"}>MY WAY</div>
-        <div className={"text-center"}>Уникальный путь  <br/>к твоему образу с My Way</div>
+        <div className={"text-center"}>Уникальный путь  <br/>к твоему образу с My Way!</div>
         {isMobile ? <LinkSolid text={"Записаться онлайн"} href={"/booking"}/> : null}
       </div>
     </>
