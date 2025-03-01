ffbdb2134f5ae7a5fa1c7ead996f555b7b388a58
rumpwar <niki4.mak@gmail.com>
2025-03-01 21:05:43 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,35 @@
 import Image from "next/image";
 import ButtonTransparent from "@/components/shared/button/button-transparent";
 import Link from "next/link";
-import { Main } from "next/document";
-import { redirect } from "next/navigation";
 
 export default async function Entry() {
-  redirect('/home');
+  // redirect('/home');
   return (
-    // <div className={"w-screen h-screen top-0 left-0 grid grid-cols-2"}>
-    //   <Link href={"/home"} className={"relative h-full w-full flex items-center justify-center transition-all bg-black hover:bg-transparent"}>
-    //     <div className={"absolute h-full right-0 flex flex-col justify-end"}>
-    //       <Image
-    //         className={"h-[80%] w-fit object-cover object-right z-0"}
-    //         width={1040}
-    //         height={1960}
-    //         src={"/landing/d-woman@2x.png"}
-    //         alt="Precedent Logo"
-    //       />
-    //     </div>
-    //     <ButtonTransparent text={"Женское"}/>
-    //   </Link>
-    //   <Link href={"/home"} className={"relative w-full h-full flex items-center justify-center transition-all bg-black hover:bg-transparent"}>
-    //     <div className={"absolute h-full left-0 flex flex-col justify-end"}>
-    //       <Image
-    //         className={"h-[80%] w-fit object-cover object-left z-0"}
-    //         width={1040}
-    //         height={1960}
-    //         src={"/landing/d-man@2x.png"}
-    //         alt="Precedent Logo"
-    //       />
-    //     </div>
-    //     <ButtonTransparent text={"Мужское"} />
-    //   </Link>
-    // </div>
-    <></>
+    <div className={"w-screen h-screen top-0 left-0 grid grid-cols-2"}>
+      <Link href={"/home"} className={"relative h-full w-full flex items-center justify-center transition-all bg-black hover:bg-transparent"}>
+        <div className={"absolute h-full right-0 flex flex-col justify-end"}>
+          <Image
+            className={"h-[80%] w-fit object-cover object-right z-0"}
+            width={1040}
+            height={1960}
+            src={"/landing/d-woman@2x.png"}
+            alt="Precedent Logo"
+          />
+        </div>
+        <ButtonTransparent text={"Женское"}/>
+      </Link>
+      <Link href={"/home"} className={"relative w-full h-full flex items-center justify-center transition-all bg-black hover:bg-transparent"}>
+        <div className={"absolute h-full left-0 flex flex-col justify-end"}>
+          <Image
+            className={"h-[80%] w-fit object-cover object-left z-0"}
+            width={1040}
+            height={1960}
+            src={"/landing/d-man@2x.png"}
+            alt="Precedent Logo"
+          />
+        </div>
+        <ButtonTransparent text={"Мужское"} />
+      </Link>
+    </div>
   );
 }
